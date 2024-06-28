c9d68d1e56c04fcd61ed759f89d11460d93bf153
iamtamrat <demsetamrat@gmail.com>
2024-06-29 00:39:45 +0300

    change

--- app.js
+++ app.js
@@ -1,75 +1,63 @@
-const ex=require('express');
-const app=ex();
+const express = require('express');
 const bodyParser = require('body-parser');
-const ht=require('https');
+const https = require('https');
 require('dotenv').config();
 
-app.use(bodyParser.urlencoded({extended:true}));
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + "/signup.html");
-})
+});
 
-
-
-
-app.post('/',function(req,res){
-    var fname=req.body.fname;
-    var lname=req.body.lname;
-    var email=req.body.email;
-     
-    var data={
-        members:[
+app.post('/', function(req, res) {
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+    const email = req.body.email;
+    
+    const data = {
+        members: [
             {
-                email_address:email,
-                status:'subscribed',
-                merge_fields:{
-                    FNAME:fname,
-                    LNAME:lname
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
                 }
             }
         ]
     };
 
-    const jsonData=JSON.stringify(data);
+    const jsonData = JSON.stringify(data);
     const AUDI_id = process.env.audi_id; // Ensure this is set correctly in your environment 
     const url = `https://us22.api.mailchimp.com/3.0/lists/${AUDI_id}`;
-    
-
     const api_key = process.env.api_key;
-    const Options = {
+    
+    const options = {
         method: 'POST',
-        auth: `tamrat:${api_key}`
+        auth: `anystring:${api_key}` // 'anystring' can be anything, Mailchimp requires it in the format 'anystring:apikey'
     };
     
-   const request= ht.request(url,Options,function(response){
-            response.on('data' ,function(data){
-                if (response.statusCode===200) {
-                    res.sendFile(__dirname + '/seccuss.html')
-                }
-                else{
-                    res.sendFile(__dirname + '/failure.html')
-                }
-            })
-
-          
+    const request = https.request(url, options, function(response) {
+        response.on('data', function(data) {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + '/success.html'); // Fixed typo from 'seccuss.html'
+            } else {
+                res.sendFile(__dirname + '/failure.html');
+            }
+        });
     });
-
 
     request.write(jsonData);
     request.end();
-
 });
 
-app.post('/failure',function(req,res){
+app.post('/failure', function(req, res) {
     res.redirect('/');
-})
+});
 
-
-app.listen(  process.env.PORT || 3000,function(){
-    console.log('server running on port 3000');
-})
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+    console.log(`Server running on port ${PORT}`);
+});
