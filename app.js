09f68494e3430eedf53d9d44d56dacb7eb444183
iamtamrat <demsetamrat@gmail.com>
2024-07-07 00:26:34 +0300

    api key problem fixing

--- app.js
+++ app.js
@@ -30,19 +30,19 @@ app.post('/', function(req, res) {
     };
 
     const jsonData = JSON.stringify(data);
-    const AUDI_id = process.env.audi_id; // Ensure this is set correctly in your environment 
+    const AUDI_id = process.env.audi_id; 
     const url = `https://us22.api.mailchimp.com/3.0/lists/${AUDI_id}`;
     const api_key = process.env.api_key;
     
     const options = {
         method: 'POST',
-        auth: `anystring:${api_key}` // 'anystring' can be anything, Mailchimp requires it in the format 'anystring:apikey'
+        auth: `anystring:${api_key}` 
     };
     
     const request = https.request(url, options, function(response) {
         response.on('data', function(data) {
             if (response.statusCode === 200) {
-                res.sendFile(__dirname + '/seccuss.html'); // Fixed typo from 'seccuss.html'
+                res.sendFile(__dirname + '/seccuss.html'); 
             } else {
                 res.sendFile(__dirname + '/failure.html');
             }
